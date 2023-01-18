0d206468103132110d4dc732aa487431dc2c6869
daveman-creator <djustin43@gmail.com>
2023-01-18 13:23:35 +0100

    Modify the content

--- src/App.js
+++ src/App.js
@@ -1,49 +1,131 @@
+/** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import randomColor from 'randomcolor';
 import { useState } from 'react';
+import Button from './Button.js';
+import Input from './Input.js';
 
+// CSS-in-JS
+const frame = css`
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  width: 500px;
+  height: 600px;
+  margin: 50px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px;
+`;
+
+const center = css`
+  text-align: center;
+`;
+
+// App Component
 export default function App() {
-  // const [color, setColor] = useState(randomColor());
-  const [color, setColor] = useState('');
+  const [randomColor, setRandomColor] = useState('');
   const [hue, setHue] = useState('');
   const [luminosity, setLuminosity] = useState('');
-  const [chosenColor, setChosenColor] = useState('');
+  const [width, setWidth] = useState('');
+  const [height, setHeight] = useState('');
+
+  // onChange Handlers
+  function onChangeHue(event) {
+    setHue(event.currentTarget.value);
+  }
+
+  function onChangeLuminosity(event) {
+    setLuminosity(event.currentTarget.value);
+  }
+
+  function onChangeWidth(event) {
+    setWidth(event.currentTarget.value);
+  }
+
+  function onChangeHeight(event) {
+    setHeight(event.currentTarget.value);
+  }
+
+  const str = <h2 css={center}>{randomColor}</h2>;
+
+  const colorBox = css`
+    background-color: ${randomColor};
+    width: ${width > 0 && width < 1000 ? width : 400}px;
+    height: ${height > 0 && height < 1000 ? height : 300}px;
+  `;
+
   return (
-    <div>
+    <div
+      style={{
+        color: '#FF0000',
+        background: 'azure',
+      }}
+      css={frame}
+    >
       <h1>Random Color Generator</h1>
-      <div
-        style={{
-          backgroundColor: '#FF0000',
-        }}
-      />
-      <button
-        onClick={() => {
-          setChosenColor(randomColor());
-        }}
-      >
-        Generate
-      </button>
-      <br />
-      <input
-        value={chosenColor}
-        onChange={(event) => {
-          setChosenColor(event.currentTarget.value);
-          console.log(randomColor.find(event.currentTarget).color);
-        }}
-      />
-      <div
-        style={{
-          backgroundColor: chosenColor,
-          width: '800px',
-          height: '400px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Color : {chosenColor}
+
+      <h3>Change color to be a certain hue and luminosity</h3>
+      <div>
+        <Input
+          htmlFor="hue"
+          text="hue"
+          type="text"
+          id="hue"
+          placeholder="green"
+          value={hue}
+          onChange={onChangeHue}
+        />
       </div>
-      <div></div>
+
+      <div>
+        <Input
+          htmlFor="luminosity"
+          text="luminosity"
+          type="text"
+          id="luminosity"
+          placeholder="light"
+          value={luminosity}
+          onChange={onChangeLuminosity}
+        />
+      </div>
+      <br />
+
+      <h3>Resize the box!</h3>
+      <div>
+        <Input
+          htmlFor="width"
+          text="width (in px)"
+          type="number"
+          id="width"
+          placeholder="500px"
+          value={width}
+          onChange={onChangeWidth}
+        />
+      </div>
+      <div>
+        <Input
+          htmlFor="height"
+          text="height (in px)"
+          type="number"
+          id="height"
+          placeholder="500px"
+          value={height}
+          onChange={onChangeHeight}
+        />
+      </div>
+      <br />
+      <br />
+
+      <Button
+        setRandomColor={setRandomColor}
+        luminosity={luminosity}
+        hue={hue}
+      />
+
+      <br />
+      <br />
+      <br />
+      <div css={colorBox}>{str}</div>
     </div>
   );
 }
